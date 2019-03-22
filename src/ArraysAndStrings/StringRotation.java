a44da675685a18b80fdff9298c090ec993dcea2d
theorist <theorist17@gmail.com>
2019-03-22 13:30:28 +0900

    solve 1-8, 1-9

--- src/ArraysAndStrings/StringRotation.java
+++ src/ArraysAndStrings/StringRotation.java
@@ -4,14 +4,28 @@ package ArraysAndStrings;
  * @author theorist
  * Assume you have a method isSubstring which checks if one word is a substring
  * of another. Given two strings, s1 and s2, write code to check if s2 is a
- * rotation of s1 using only one call to isSubstring (e.g., "waterbottle" is
+ * rotation of s1 using only one call to isSubstring (e.g., "waterbottlewaterbottle" is
  * a rotation of "erbottlewat").
  */
 public class StringRotation {
 
+	public static boolean isSubstring(String s1, String s2) {
+		if(s2.contains(s1))
+			return true;
+		else
+			return false;
+	}
+	public static boolean isRotation(String s1, String s2) {
+		if(isSubstring(s1, s2+s2))
+			return true;
+		else
+			return false;
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println(isSubstring("abc", "xyzabcdef"));
+		System.out.println(isRotation("waterbottle", "erbottlewat"));
+		
 	}
 
 }
--- src/ArraysAndStrings/ZeroMatrix.java
+++ src/ArraysAndStrings/ZeroMatrix.java
@@ -1,13 +1,107 @@
 package ArraysAndStrings;
 
 /**
- * @author theorist
- * Write an algorithm such that if an element in an MxN matrix is 0, 
- * its entire row and column are set to 0.
+ * @author theorist Write an algorithm such that if an element in an MxN matrix
+ *         is 0, its entire row and column are set to 0.
  */
+
+class Node {
+	int x;
+	int y;
+	Node below;
+	public Node(int x, int y, Node below) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.below = below;
+	}
+	public Node(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	public void print() {
+		System.out.println("("+x+", "+y+")");
+	}
+}
+class Stack {
+	Node top = null;
+	int length = 0;
+	void push(int x, int y) {
+		 Node newTop = new Node(x, y, top);
+		 top = newTop;
+		 length++;
+	}
+	Node pop() {
+		if(top==null)
+			return null;
+		Node temp = top;
+		top = top.below;
+		length--;
+		return temp;
+	}
+}
 public class ZeroMatrix {
 
+	static Stack coordZero(int[][] matrix) {
+		Stack stack = new Stack();
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				if(matrix[i][j]==0) {
+					stack.push(j, i);
+				}
+			}
+		}
+		
+		return stack;
+	}
+
+	static void extendZero(int [][]matrix, int x, int y, int dx, int dy) {
+		if(x<0||y<0||x==matrix[0].length||y==matrix.length) {
+			return;
+		} else {
+			matrix[y][x] = 0;
+			extendZero(matrix, x+dx, y+dy, dx, dy);
+		}
+		
+	}
+	
+	static int[][] matrix(int M, int N){
+		int[][] matrix = new int[M][N];
+		
+		int index = -10;
+		for(int i = 0; i < M; i++) {
+			for(int j = 0 ; j < N; j++) {
+				matrix[i][j] = index%27;
+				index++;
+			}
+		}
+		return matrix;
+	}
+	
+	static void print(int[][]matrix) {
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0 ; j < matrix[0].length; j++) {
+				System.out.print(matrix[i][j]+"\t");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+	
 	public static void main(String[] args) {
+		int[][]matrix = matrix(10, 6);
+		print(matrix);
+		Stack stack = coordZero(matrix);
+		while(stack.top!=null) {
+			Node node = stack.pop();
+			extendZero(matrix, node.x, node.y, 1, 0);
+			extendZero(matrix, node.x, node.y, -1, 0);
+			extendZero(matrix, node.x, node.y, 0, 1);
+			extendZero(matrix, node.x, node.y, 0, -1);
+		}
+		print(matrix);
+		
 	}
 
 }
